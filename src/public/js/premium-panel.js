3d2600735449c1b69da14cb303aaabe52987cd8f
Juan Mansur <juancruzmansur@hotmail.com>
2024-06-19 13:41:15 +0200

    ProyecTest

--- src/public/js/premium-panel.js
+++ src/public/js/premium-panel.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io();
 
-     // Manejar la solicitud POST del formulario
      document.getElementById('productForm').addEventListener('submit', async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
@@ -17,16 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
             body: JSON.stringify(productData)
         });
         if (response.ok) {
-            // Limpiar el formulario después de agregar el producto
             event.target.reset();
         }
     });
 
-    // Manejar actualizaciones en tiempo real de la lista de productos
     socket.on('productos', (productos) => {
-        // Actualizar la lista de productos del usuario
         const productList = document.getElementById('userProductsList');
-        productList.innerHTML = ''; // Limpiar la lista antes de agregar los nuevos productos
+        productList.innerHTML = ''; 
         productos.forEach(product => {
             const newItem = document.createElement('li');
             newItem.innerHTML = `
@@ -41,7 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-    // Manejar la eliminación de productos
     document.getElementById('userProductsList').addEventListener('click', async (event) => {
         if (event.target.classList.contains('deleteButton')) {
             const productId = event.target.parentElement.dataset.productId;
@@ -49,13 +44,11 @@ document.addEventListener('DOMContentLoaded', () => {
                 method: 'DELETE'
             });
             if (response.ok) {
-                // Eliminar el producto de la lista después de eliminarlo del servidor
                 event.target.parentElement.parentElement.remove();
             }
         }
     });
 
-    // Manejar actualizaciones en tiempo real de la lista de productos
     socket.on('productos', (productos) => {
         const productList = document.getElementById('userProductsList');
         productList.innerHTML = '';
